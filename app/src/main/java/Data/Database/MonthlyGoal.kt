0d3e0de9f554cb254fdfb284d5e35ff0b385c7ff
ammaarahk05 <ST10447616@vcconnect.edu.za>
2026-04-25 00:17:30 +0200

    Added missing Data folder from master

---
+++ app/src/main/java/Data/Database/MonthlyGoal.kt
@@ -0,0 +1,12 @@
+package com.example.budgetquest.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="monthly_goals")
+data class MonthlyGoal (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val minGoal: Double,
+    val maxGoal: Double
+)

1fc3923f28a69ff3327637a9bfe802aaa213a7db
Nabihah Sema <ST10440590@myemeris.edu.za>
2026-04-24 20:28:59 +0200

    Created front-end Login page and packages to connect to the database

---
+++ app/src/main/java/Data/Database/MonthlyGoal.kt
@@ -0,0 +1,4 @@
+package Data.Database
+
+class MonthlyGoal {
+}
---
+++ app/src/main/java/com/example/budgetquest/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.budgetquest
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    //global declarations
+    private lateinit var etMainName: EditText
+    private lateinit var etMainPassword: EditText
+    private lateinit var btnMainLogin: Button
+    private lateinit var btnMainSignUp: Button
+
+    private lateinit var db: AppDatabase
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
